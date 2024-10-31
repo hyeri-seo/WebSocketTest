5156144cf41ad66b7794c0a8edc1b9224a196ebf
hyeri-seo <141993427+hyeri-seo@users.noreply.github.com>
2024-10-31 23:38:45 +0900

    초기 생성

---
+++ src/main/java/org/mind/websockettest/WebSocketTestApplication.java
@@ -0,0 +1,13 @@
+package org.mind.websockettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mind/websockettest/WebSocketTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mind.websockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
